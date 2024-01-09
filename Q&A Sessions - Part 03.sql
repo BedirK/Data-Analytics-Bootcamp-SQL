2b197c957be381e3d350d79d9cbaf8b514b6ee98
Bedirhan Kelez <103532330+BedirK@users.noreply.github.com>
2024-01-09 17:09:52 +0300

    Update Q&A Sessions - Part 03.sql

--- Q&A Sessions - Part 03.sql
+++ Q&A Sessions - Part 03.sql
@@ -1,86 +1,74 @@
---Soru: "Orders" tablosunu kullanarak, siparişlerin hangi aylarda yapıldığını ve her ay kaç sipariş olduğunu
-bulan bir SQL sorgusu nasıl oluşturulur?
+--Soru: "Orders" tablosunu kullanarak, siparişlerin hangi aylarda yapıldığını ve her ay kaç sipariş olduğunu bulan bir SQL sorgusu oluşturunuz.
+----Question: Using the "Orders" table, write a SQL query that finds the months in which orders were made and how many orders there were in each month.
 
 SELECT
-DATEPART(month, o.OrderDate) AS OrderMonth,
-COUNT(*) AS OrderCount
-FROM
-Orders o
-GROUP BY
-DATEPART(month, o.OrderDate)
-ORDER BY
-OrderMonth;
+    DATEPART(month, o.OrderDate) AS OrderMonth,
+    COUNT(*) AS OrderCount
+FROM Orders o
+GROUP BY DATEPART(month, o.OrderDate)
+ORDER BY OrderMonth;
 
---Soru: Ürünleri fiyatlarına göre sıralayarak her ürünün fiyat sıralama numarasını bulun..
+--Soru: Ürünleri fiyatlarına göre sıralayarak her ürünün fiyat sıralama numarasını bulun.
+--Question: Sort the products by price and find the price ranking number of each product.
 
-SELECT
-ProductName,
-UnitPrice,
-RANK() OVER (ORDER BY UnitPrice) AS PriceRank
-FROM
-Products
-ORDER BY
-UnitPrice;
+SELECT ProductName,
+        UnitPrice,
+        RANK() OVER (ORDER BY UnitPrice) AS PriceRank
+FROM    Products
+ORDER BY  UnitPrice;
 
-**RANK() ile ROW_NUMBER() FARKI NEDIR?
+--Soru: Siparişleri sipariş tutarına göre eşit parçalara bölün (dörtte birlik dilimlere ayırın).
+--Question: Divide orders into equal parts (quartiles) according to the order amount.
 
---Soru: Siparişleri sipariş tutarına göre eşit parçalara bölün (dörtte birlik dilimlere ayırın)..
-
-SELECT
-OrderID,
-CustomerID,
-OrderDate,
-Freight,
+SELECT  OrderID,
+        CustomerID,
+        OrderDate,
+        Freight,
 NTILE(4) OVER (ORDER BY Freight) AS FreightQuartile
-FROM
-Orders
-ORDER BY
-Freight;
+FROM Orders
+ORDER BY Freight
 
---Soru: Siparişleri sipariş tutarına göre eşit parçalara bölün (dörtte birlik dilimlere ayırın) ve 4 segment
-oluşturun. (Dusuk, Orta, Yuksek, Cok Yuksek)
+--Soru: Siparişleri sipariş tutarına göre eşit parçalara bölün (dörtte birlik dilimlere ayırın) ve 4 segment oluşturun. (Dusuk, Orta, Yuksek, Cok Yuksek)
+--Question: Divide orders into equal parts (quartiles) based on order amount and 4 segments (Low, Medium, High, Very High)
 
-SELECT
-OrderID,
-CustomerID,
-OrderDate,
-Freight,
+SELECT  OrderID,
+        CustomerID,
+        OrderDate,
+        Freight,
 CASE
-WHEN NTILE(4) OVER (ORDER BY Freight) = 1 THEN 'DUSUK'
-WHEN NTILE(4) OVER (ORDER BY Freight) = 2 THEN 'ORTA'
-WHEN NTILE(4) OVER (ORDER BY Freight) = 3 THEN 'YUKSEK'
-WHEN NTILE(4) OVER (ORDER BY Freight) = 4 THEN 'COK YUKSEK'
+    WHEN NTILE(4) OVER (ORDER BY Freight) = 1 THEN 'DUSUK'
+    WHEN NTILE(4) OVER (ORDER BY Freight) = 2 THEN 'ORTA'
+    WHEN NTILE(4) OVER (ORDER BY Freight) = 3 THEN 'YUKSEK'
+    WHEN NTILE(4) OVER (ORDER BY Freight) = 4 THEN 'COK YUKSEK'
+  
 END AS FreightQuartile
-FROM
-Orders
-ORDER BY
-Freight;
+FROM Orders
+ORDER BY Freight
 
 --Soru: Ürünleri birim fiyatlarına göre ucuz (<10), orta fiyatlı ve pahalı (>50) olarak sınıflandırın.
+--Question: Categorize products according to their unit price as cheap (<10), medium priced and expensive (>50).
 
-SELECT
-ProductName,
-UnitPrice,
+SELECT  ProductName,
+        UnitPrice,
 CASE
-WHEN UnitPrice < 10 THEN 'Ucuz'
-WHEN UnitPrice BETWEEN 10 AND 50 THEN 'Orta Fiyatlı'
-WHEN UnitPrice > 50 THEN 'Pahalı'
+    WHEN UnitPrice < 10 THEN 'Ucuz'
+    WHEN UnitPrice BETWEEN 10 AND 50 THEN 'Orta Fiyatlı'
+    WHEN UnitPrice > 50 THEN 'Pahalı'
 END AS PriceCategory
-FROM
-Products;
+FROM Products
 
 --Soru: Müşterilerin iletişim adreslerinin başına Adres: ekleyiniz.
+--Question: Add "Address:" at the beginning of the customers' contact addresses.
 
-SELECT
-ContactName,
-Address,
-STUFF(Address, 1, 0, 'Adres: ') AS ModifiedAddress
-FROM
-Customers;
+SELECT  ContactName,
+        Address,
+        STUFF(Address, 1, 0, 'Adres: ') AS ModifiedAddress
+FROM    Customers;
 
 ---CTE (COMMON TABLE EXPRESSIONS)
 
-Soru: Bu kodu Common Table Expression örneği ile yazınız.
+-- Soru: Aşağıdaki kodu Common Table Expression örneği ile yazınız.
+-- Question: Write the following code by using Common Table Expression
 
 SELECT EmployeeID CALISAN, YEAR(OrderDate) YIL,SUM(Freight) YIL_CIRO FROM Orders
 GROUP BY EmployeeID, YEAR(OrderDate)
